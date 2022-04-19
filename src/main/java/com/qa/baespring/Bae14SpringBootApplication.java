557f9beffa7e2d8b00f6992450ec84df98389fa1
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-19 15:46:35 +0100

    first commit

---
+++ src/main/java/com/qa/baespring/Bae14SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.qa.baespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bae14SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bae14SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qa/baespring/domain/User.java
@@ -0,0 +1,12 @@
+package com.qa.baespring.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+	
+	private String firstname;
+	private String lastname;
+	private String username;
+
+}
