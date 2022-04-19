6dae086304deb2fc33f810b4817d946f1d424f58
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-19 16:29:09 +0100

    adding the h2 database to the application.properties file

--- src/main/java/com/qa/baespring/domain/User.java
+++ src/main/java/com/qa/baespring/domain/User.java
@@ -1,12 +1,112 @@
 package com.qa.baespring.domain;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	// Creates a column called "firstname"
+	@Column(name = "firstname")
 	private String firstname;
+	
+	// Creates a column called "last_name"
+
 	private String lastname;
+	
+	// Creates a column called "username"
+	// This column CAN NOT be null
+	@Column(nullable = false)
 	private String username;
+
+	// Default Constructor
+	public User() {}
+	
+	
+	// Used for creating/inserting
+	public User(String firstname, String lastname, String username) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.username = username;
+	}
+
+	// Used for reading/selecting (and testing)
+	public User(long id, String firstname, String lastname, String username) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.username = username;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", username=" + username
+				+ "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstname, id, lastname, username);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(firstname, other.firstname) && id == other.id && Objects.equals(lastname, other.lastname)
+				&& Objects.equals(username, other.username);
+	}
+	
+	
 
 }
