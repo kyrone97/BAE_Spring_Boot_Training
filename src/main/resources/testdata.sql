c4bce7e51b00d040e640a60ab89f55079b663893
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-04-22 12:51:13 +0100

    added testing to API one test post test

---
+++ src/main/resources/testdata.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`firstname`, `lastname`, `username`, `age`) VALUES ('Kyrone', 'James', 'kjames1', 24);
---
+++ src/test/java/com/qa/baespring/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.qa.baespring.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.baespring.domain.User;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
+public class UserControllerIntegrationTest {
+	
+
+	@Autowired
+	private MockMvc mvc;
+	
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	
+	@Test // test are void because they dont return anything
+	public void createTest() throws Exception {
+		
+	User entry = new User("Jim", "Jones", "JJones1",24);
+	String entryAsJSON = mapper.writeValueAsString(entry);
+	
+	User result = new User(2L, "Jim", "Jones", "JJones1",24);
+	String resultAsJSON = mapper.writeValueAsString(result);
+	
+	mvc.perform(post("/user/create")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(entryAsJSON))
+			.andExpect(status().isCreated())
+			.andExpect(content().json(resultAsJSON));
+	
+	}
+	
+	
+
+}
